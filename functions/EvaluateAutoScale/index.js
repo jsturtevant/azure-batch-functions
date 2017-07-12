d8a72e1ada0ec17113c412bd0886779bb9367a92
James Sturtevant <jsturtevant@gmail.com>
2017-07-12 16:03:19 -0400

    initial pass at full testing

--- functions/EvaluateAutoScale/index.js
+++ functions/EvaluateAutoScale/index.js
@@ -1,13 +1,13 @@
-var batch = require('azure-batch');
-var helpers = require('../helpers/helpers.js');
-var os = require("os");
-var moment = require('moment');
-var validator = require('validator');
+const batch = require('azure-batch'),
+      helpers = require('../helpers/helpers'),
+      validator = require('validator'),
+      os = require("os"),
+      AutoScaleEvaluator = require('./autoScaleEvaluator');
 
 module.exports = function (context, req) {
     context.log('processing...');
     
-    var batch_client = helpers.batchClientFactory();
+    const batch_client = helpers.batchClientFactory();
     
     if (!req.body.poolid || validator.isEmpty(req.body.poolid)){
         context.log("Invalid response");
@@ -16,72 +16,31 @@ module.exports = function (context, req) {
         return;
     }
 
-    var poolId = req.body.poolid;
-    var maxNodes = req.body.maxNodes;
+    if (!req.body.maxNodes || validator.isEmpty(req.body.maxNodes) || !validator.isNumeric(req.body.maxNodes)){
+        context.log("Invalid response");
+        context.res = { status: 400, body: 'must pass maxNodes' }; 
+        context.done();
+        return;
+    }
 
-    batch_client.pool.get(poolId).then((poolInfo) => {
-        context.log(`pool state: ${poolInfo.state}`);
-        return isAutoScaleEnabled(poolInfo);
-    }).then(poolInfo => {
-        context.log(`Auto Scale: ${poolInfo.enableAutoScale}`);
-        return ensureAutoScaleSet(batch_client, poolInfo);
-    }).then(_ => {
-        return evaluateAutoScale(batch_client, poolId, maxNodes);
-    }).then(evalResult => {
+    const poolId = req.body.poolid;
+    const maxNodes = req.body.maxNodes;
+
+    let evaluator =  new AutoScaleEvaluator(batch_client.pool);
+
+    evaluator.evaluateAutoScale(poolId, maxNodes).then(evalResult => {
         context.log("Auto Scale Results:");
         context.log(evalResult.results.replace("\$/gi", os.EOL + "\t$"));
+        context.res = {status:200};
         context.done();
     }).catch(err => {
         context.log('An error occurred.');
         printErrors(context, err);
+        context.res = {status:500};
         context.done();
     });
-
 };
 
-function isAutoScaleEnabled(poolInfo){
-    if(poolInfo.state != "active")
-    {
-        return Promise.reject({code: "notActive"});
-    }
-
-    return Promise.resolve(poolInfo);
-}
-
-function ensureAutoScaleSet(batch_client, poolInfo){
-    if (poolInfo.enableAutoScale == true)
-    {
-        return Promise.resolve();
-    }
-
-    var autoScaleProperties ={
-        autoScaleFormula: `$TargetLowPriorityNodes = ${poolInfo.currentLowPriorityNodes};`,
-        autoScaleEvaluationInterval: moment.duration(5, 'minutes')
-    };
-
-    //enable if first and set to current.
-    return batch_client.pool.enableAutoScale(poolInfo.id, autoScaleProperties)
-}
-
-function evaluateAutoScale(batch_client, poolId, maxNodes){
-       var myFormula = `maxNodes 		 =  ${maxNodes};
-
-// Get pending tasks for the past 15 minutes.
-// If we have fewer than 70 percent data points, we use the last sample point,
-// otherwise we use the maximum of last sample point and the history average.
-$samples = $ActiveTasks.GetSamplePercent(TimeInterval_Minute * 15);
-$tasks = $samples < 70 ? max(0,$ActiveTasks.GetSample(1)) : max( $ActiveTasks.GetSample(1), avg($ActiveTasks.GetSample(TimeInterval_Minute * 15)));
-
-// If number of pending tasks is not 0, set targetVM to pending tasks, otherwise
-// half of current LowPriority.
-// The pool size is capped at maxNodes (4), if target VM value is more than that, set it
-// to maxNodes. This value should be adjusted according to your use case.
-$targetVMs = $tasks > 0? $tasks:max(0, $TargetLowPriorityNodes/2);
-
-$TargetLowPriorityNodes = max(0, min($targetVMs, maxNodes));`;
-
-        return batch_client.pool.evaluateAutoScale(poolId, myFormula);
-}
 
 function printErrors(context, err){
     if (err.body)
--- tests/evaluateautoscale.test.js
+++ tests/evaluateautoscale.test.js
@@ -1,12 +1,16 @@
-var test = require('tape-catch');
-var td = require('testdouble');
+const test = require('tape-catch'),
+        td = require('testdouble'),
+        funcHarness = require('azure-functions-node-harness');
 
-var funcHarness = require('azure-functions-node-harness');
-var reqBuilder = require('azure-functions-node-harness/src/request-builder');
-var helpers = td.replace('../functions/helpers/helpers.js');
+// overrides
+const helpers = td.replace('../functions/helpers/helpers');
+const AutoScaleEvaluator = td.replace('../functions/EvaluateAutoScale/autoScaleEvaluator');
 
 test('Evaluate AutoScale Tests', function (group) {
-    var funcToTest = funcHarness('EvaluateAutoScale', { dirname: 'functions' });
+    const funcToTest = funcHarness('EvaluateAutoScale', { dirname: 'functions' });
+
+    td.when(helpers.batchClientFactory()).thenReturn({pool: {}});
+
 
     group.test('if poolid is empty then return status 400', function (t) {
         t.plan(1);
@@ -28,10 +32,118 @@ test('Evaluate AutoScale Tests', function (group) {
 
         funcToTest.invokeHttpTrigger({
             reqBody: {
+                "poolid": null,
                 "maxNodes": 4
             }
         }).then(context => {
             t.equal(400, context.res.status);
+        }).catch(err => {
+            t.fail(`something went wrong: ${err}`);
+        });
+    });
+
+    group.test('if poolid is missing then return status 400', function (t) {
+        t.plan(1);
+
+        funcToTest.invokeHttpTrigger({
+            reqBody: {
+                "maxNodes": 4
+            }
+        }).then(context => {
+            t.equal(400, context.res.status);
+        }).catch(err => {
+            t.fail(`something went wrong: ${err}`);
+        });
+    });
+
+    group.test('if maxnodes is empty then return status 400', function (t) {
+        t.plan(1);
+
+        funcToTest.invokeHttpTrigger({
+            reqBody: {
+                "poolid": "testpool",
+                "maxNodes": ""
+            }
+        }).then(context => {
+            t.equal(400, context.res.status);
+        }).catch(err => {
+            t.fail(`something went wrong: ${err}`);
+        });
+    });
+
+    group.test('if maxnodes is null then return status 400', function (t) {
+        t.plan(1);
+
+        funcToTest.invokeHttpTrigger({
+            reqBody: {
+                "poolid": "testpool",
+                "maxNodes": null
+            }
+        }).then(context => {
+            t.equal(400, context.res.status);
+        }).catch(err => {
+            t.fail(`something went wrong: ${err}`);
+        });
+    });
+
+    group.test('if maxnodes is missing then return status 400', function (t) {
+        t.plan(1);
+
+        funcToTest.invokeHttpTrigger({
+            reqBody: {
+                "poolid": "testpool"
+            }
+        }).then(context => {
+            t.equal(400, context.res.status);
+        }).catch(err => {
+            t.fail(`something went wrong: ${err}`);
+        });
+    });
+
+    group.test('if maxnodes not a number return 400', function (t) {
+        t.plan(1);
+
+        funcToTest.invokeHttpTrigger({
+            reqBody: {
+                "poolid": "testpool",
+                "maxNodes": "some"
+            }
+        }).then(context => {
+            t.equal(400, context.res.status);
+        }).catch(err => {
+            t.fail(`something went wrong: ${err}`);
+        });
+    });
+
+    group.test('if valid parameters should return 200', function (t) {
+        t.plan(1);
+
+        td.when(AutoScaleEvaluator.prototype.evaluateAutoScale("testpool", "1")).thenResolve({results:""})
+
+        funcToTest.invokeHttpTrigger({
+            reqBody: {
+                "poolid": "testpool",
+                "maxNodes": "1"
+            }
+        }).then(context => {
+            t.equal(200, context.res.status);
+        }).catch(err => {
+            t.fail(`something went wrong: ${err}`);
+        });
+    });
+
+    group.test('if valid parameters but evaluator throws exception then 500', function (t) {
+        t.plan(1);
+
+        td.when(AutoScaleEvaluator.prototype.evaluateAutoScale("testpool", "1")).thenReject({err:"something blew up"})
+
+        funcToTest.invokeHttpTrigger({
+            reqBody: {
+                "poolid": "testpool",
+                "maxNodes": "1"
+            }
+        }).then(context => {
+            t.equal(500, context.res.status);
         }).catch(err => {
             t.fail(`something went wrong: ${err}`);
         });
